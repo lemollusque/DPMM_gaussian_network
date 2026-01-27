4a3e7a6e597f6b918946a860e532a36f8ef420a4
bastian widmer <lemolllusque@gmail.com>
2026-01-27 11:00:36 +0100

    Add no parents likelihood

--- proof_of_concept_with_gaussians.R
+++ proof_of_concept_with_gaussians.R
@@ -18,7 +18,7 @@ N <- 100  # number of samples
 x1 <- rnorm(N, mean=sample(1:10)[1], sd=sample(1:10)[1]) # TODO check wiith 0 ,1 
 x2 <- rnorm(N, mean=sample(1:10)[1], sd=sample(1:10)[1])
 x3 <- rnorm(N, mean=sample(1:10)[1], sd=sample(1:10)[1])
-x4 <-rnorm(N, mean=sample(1:10)[1], sd=sample(1:10)[1])
+x4 <- rnorm(N, mean=sample(1:10)[1], sd=sample(1:10)[1])
 x5 <- 1.2 * x1 - 0.8 * x2 + rnorm(N)
 
 data <- data.frame(x1, x2, x3, x4, x5)
@@ -75,7 +75,7 @@ loglik_cond_component_dp <- function(dp_data, mu, Sigma) {
   Z = backsolve(t(L), W)
   S_cond = S_yy - as.numeric(S_yx %*% Z)
     
-  # per-observation log density
+  # log density
   dnorm(y, mean = mu_cond, sd = sqrt(S_cond), log = TRUE)
   
 }
@@ -87,7 +87,7 @@ loglik_cond_dp <- function(dp_data, pis, mus, Sigmas) {
   
   logpis <- log(pis)
   
-  # n x K matrix: entry (i,k) = log pi_k + log p_k(y_i | x_i)
+  #entry (i,k) = log pi_k + log p_k(y_i | x_i)
   ll_mat <- matrix(NA_real_, nrow = n, ncol = K)
   for (k in 1:K) {
     ll_mat[, k] <- logpis[k] + loglik_cond_component_dp(dp_data, mus[, , k], Sigmas[, , k])
@@ -98,10 +98,58 @@ loglik_cond_dp <- function(dp_data, pis, mus, Sigmas) {
   sum(m + log(rowSums(exp(ll_mat - m))))
 }
 
+loglik_dp <- function(dp_data, pis, mus, Sigmas) {
+  n <- nrow(dp_data)
+  K <- length(pis)
+  
+  logpis <- log(pis)
+  
+  #entry (i,k) = log pi_k + log p_k(y_i | x_i)
+  ll_mat <- matrix(NA_real_, nrow = n, ncol = K)
+  for (k in 1:K) {
+    ll_mat[, k] <- logpis[k] + dnorm(dp_data, mean = mus[, , k], sd = sqrt(Sigmas[, , k]), log = TRUE)
+  }
+  
+  # total log-likelihood: sum_i logsumexp_k ll_mat[i,k]
+  m <- apply(ll_mat, 1, max)
+  sum(m + log(rowSums(exp(ll_mat - m))))
+}
+
+dp_ll = function(dat, n_iter){
+  #first column = child node
+  # remaining parents
+  if (ncol(dat)<2){
+    
+    dp <- DirichletProcessGaussian(dat)
+    dp <- Fit(dp, n_iter)
+    
+    pis    <- dp$weights                     
+    mus    <- dp$clusterParameters[[1]]         
+    Sigmas <- dp$clusterParameters[[2]]  
+    ll = loglik_dp(dat, pis, mus, Sigmas)
+    
+    
+    
+  }
+  else{
+    dp <- DirichletProcessMvnormal(dat)
+    dp <- Fit(dp, n_iter)
+    
+    pis    <- dp$weights                     
+    mus    <- dp$clusterParameters$mu         
+    Sigmas <- dp$clusterParameters$sig   
+    ll = loglik_cond_dp(dat, pis, mus, Sigmas)
+  }
+  ll
+}
+
+
+
 ll = list()
 bic = list()
 
 parents <- list(
+  c(),
   c("x1"),
   c("x2"),
   c("x1","x2"),
@@ -113,15 +161,10 @@ for(i in 1:length(parents)){
   set.seed(101)
   pax = parents[[i]]
   dp_data = scale(data[ ,c("x5" , pax)]) 
-  dp <- DirichletProcessMvnormal(dp_data)
-  dp <- Fit(dp, 200)
   
-  pis    <- dp$weights                     
-  mus    <- dp$clusterParameters$mu         
-  Sigmas <- dp$clusterParameters$sig   
   
   # loglikelihood
-  ll[[i]] <- loglik_cond_dp(dp_data, pis, mus, Sigmas)
+  ll[[i]] <- dp_ll(dp_data, 10)
   
   n <- nrow(dp_data)
   d <- ncol(dp_data)              
@@ -139,235 +182,5 @@ p <- exp(post - logSumExp(post))
 plot(1:length(parents), p)
 
 
-# compare means and covs
-pax = parents[[4]]
-dp_data = scale(data[ ,c("x5" , pax)])
 
 
-dp <- DirichletProcessMvnormal(dp_data)
-dp <- Fit(dp, 2000)
-                   
-mus    <- dp$clusterParameters$mu         
-Sigmas <- dp$clusterParameters$sig 
-
-Delta <- Sigmas[,,1] - cov(dp_data)
-
-cov(dp_data)
-mus
-Sigmas
-Delta
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-cond_loglik_from_joint <- function(dp, y, X) {
-  #Standardize joint data 
-  Z  <- scale(cbind(X, y))
-  d  <- ncol(as.matrix(X))
-  Xs <- Z[, 1:d, drop = FALSE]
-  ys <- Z[, d + 1]
-  
-  N <- nrow(Xs)
-  
-  #Extract fitted mixture parameters 
-  pis    <- dp$weights                      # length K
-  mus    <- dp$clusterParameters$mu         # list, each length d+1
-  Sigmas <- dp$clusterParameters$sig        # list, each (d+1)x(d+1)
-  
-  K <- length(pis)
-  stopifnot(length(mus)/ncol(Z) == K, length(Sigmas)/(ncol(Z)^2) == K)
-  
-  logp <- numeric(N)
-  
-  #Loop over observations 
-  for (i in 1:N) {
-    
-    xi <- matrix(Xs[i, ], nrow = d)
-    
-    log_w_unnorm <- numeric(K)
-    log_comp_y   <- numeric(K)
-    
-    # Loop over mixture components 
-    for (k in 1:K) {
-      
-      mu  <- mus[,,k]
-      Sig <- Sigmas[,,k]
-      
-      ## Partition mean and covariance
-      mu_x <- matrix(mu[1:d], nrow = d)
-      mu_y <- mu[d + 1]
-      
-      S_xx <- Sig[1:d,     1:d,     drop = FALSE]
-      S_xy <- Sig[1:d,     d + 1,   drop = FALSE]
-      S_yx <- Sig[d + 1,   1:d,     drop = FALSE]
-      S_yy <- Sig[d + 1,   d + 1]
-      
-      #x-dependent mixture weights 
-      log_px <- mvtnorm::dmvnorm(
-        x     = t(xi),
-        mean  = as.numeric(mu_x),
-        sigma = S_xx,
-        log   = TRUE
-      )
-      
-      log_w_unnorm[k] <- log(pis[k]) + log_px
-      
-      # Conditional Gaussian y | x 
-      invSxx <- solve(S_xx)
-      
-      m <- mu_y + as.numeric(S_yx %*% invSxx %*% (xi - mu_x))
-      V <- as.numeric(S_yy - S_yx %*% invSxx %*% S_xy)
-      
-      ## numerical safety
-      V <- max(V, 1e-10)
-      
-      log_comp_y[k] <- dnorm(
-        ys[i],
-        mean = m,
-        sd   = sqrt(V),
-        log  = TRUE
-      )
-    }
-    
-    #Mixture log-density ------------------------------------------
-    log_w <- log_w_unnorm - matrixStats::logSumExp(log_w_unnorm)
-    
-    logp[i] <- matrixStats::logSumExp(log_w + log_comp_y)
-  }
-  
-  #Return conditional log score -----------------------------------
-  sum(logp)
-}
-
-
-
-
-##############################################################
-# List all DAGs with n nodes
-all.dags <- list()
-adj <- matrix(0, nrow = n, ncol = n)
-dag.counter <- 0
-all.comb <- rep(list(c(0,1)), n*(n-1))
-all.comb <- expand.grid(all.comb)  # all combinations outside of diagonal of adjacency matrix
-
-for(i in 1:nrow(all.comb)) {
-  adj[col(adj)!=row(adj)] <- as.numeric(all.comb[i, ])
-  
-  if(is.DAG(adj)) {
-    dag.counter <- dag.counter + 1
-    all.dags[[dag.counter]] <- adj
-  }
-}
-
-# Compute true posterior (and save all scores)
-true.post <- rep(NA, dag.counter)
-parent.scores <- data.frame(sets = character(0), newscore = character(0))  
-parent.scores <- rep(list(parent.scores), n)
-
-for(k in 1:dag.counter) {
-  print(k)
-  dag <- all.dags[[k]]
-  curr_score <- 0
-  
-  for(x in 1:n) {
-    print(x)
-    set <- parent.scores[[x]]$sets
-    pax <- dag[,x]
-    pax.str <- paste(pax, collapse = "")  # parents of x
-    check <- is.na(match(set, pax.str))  # check if parent set is already there
-    
-    if(all(check)) {  # new parent set
-      
-      if(sum(pax) == 0) {  # Compute local score
-        print("no pax")
-        dp <- DirichletProcessGaussian(scale(data[,x]))
-        dp <- Fit(dp, 200)
-        loc_score <- sum(log(Predictive(dp$mixingDistribution, scale(data[,x]))))
-      }
-      else {
-        print("pax")
-        dp <- DirichletProcessMvnormal(scale(cbind(data[ ,which(pax==1)], data[,x])))
-        dp <- Fit(dp, 200)
-        loc_score <- cond_loglik_from_joint(dp, data[,x], data[ ,which(pax==1)])
-      }
-      parent.scores[[x]][length(set)+1, ] <- c(pax.str, loc_score)
-    }
-    
-    else {  # fetch score from parent.scores
-      ind <- which(check == F)  # index of pax in set
-      loc_score <- as.numeric(parent.scores[[x]]$newscore[ind])
-    }
-    curr_score <- curr_score + loc_score  # build score
-  }
-  true.post[k] <- curr_score
-}
-
-true.p <- exp(true.post - logSumExp(true.post))
-
-
-#compute shd for every graph
-shd <- rep(NA, dag.counter)
-count_edges <- rep(NA, dag.counter)
-for(k in 1:dag.counter) {
-  print(k)
-  dag <- all.dags[[k]]
-  camparison = compareDAGs(dag, truegraph)
-  shd[k] = camparison["SHD"]
-  count_edges[k] = sum(dag)
-}
-shd.order =  order(shd, decreasing = F) 
-a <- true.post[shd.order]
-b <- true.p[shd.order]
-
-plot(a, type="l")
-plot(b, type="l")
-
-
-
-
-##############################################################
-false.post = true.post[2:dag.counter]
-false.p =   exp(false.post - logSumExp(false.post))
-#compute shd for every graph
-shd <- rep(NA, dag.counter)
-count_edges <- rep(NA, dag.counter)
-for(k in 2:dag.counter) {
-  print(k)
-  dag <- all.dags[[k]]
-  camparison = compareDAGs(dag, truegraph)
-  shd[k] = camparison["SHD"]
-  count_edges[k] = sum(dag)
-}
-shd.order =  order(shd, decreasing = F) 
-a <- false.post[shd.order]
-b <- false.p[shd.order]
-
-plot(a, type="l")
-plot(b, type="l")
