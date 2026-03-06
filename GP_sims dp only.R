6d810dd5842234327d0870c65587e23eb49a1615
bastian widmer <lemolllusque@gmail.com>
2026-03-06 11:12:03 +0100

    Compare dps and bges

--- GP_sims dp only.R
+++ GP_sims dp only.R
@@ -1,15 +1,21 @@
 library(BiDAG)
 library(matrixStats)
+library(dirichletprocess)
+library(dplyr)
+library(ggplot2)
 
 source("Fourier_fns.R")
-source("BayesStanFns.R")
-source("sampling_fns.R")
 source("comparison_algs.R")
 source("dualPC.R")
-insertSource("GPscore.R", package = "BiDAG")
+source("fns.R")
+# load functions script
+insertSource("fns.R", package = "BiDAG")
+
+
+
 
 init.seed <- 100
-iter <- 100  # number of simulations
+iter <- 3  # number of simulations
 lambdas <- c(0, 0.5, 1)  # non-linearity; zero is linear
 dual <- T    # use dualPC
 n <- 10      # number of nodes
@@ -17,22 +23,18 @@ N <- 100     # number of samples
 results <- data.frame()
 
 # Parameters for ROC curves
-bge.mus <- c(0.01, 0.1, 0.5, 2, 5)
-gp.pars <- c(0.1, 0.5, 1, 10, 100)  # 0.1 is very slow
-dib.pars <- c(1, 1.25, 1.5, 1.75, 2)
-pc.pars <- c(0.004, 0.01, 0.05, 0.12, 0.3)
+bge.mus <- c(1)
 
 print(lambdas)
 
 for(lambda in lambdas) {
   print(paste("Lambda:", lambda))
 
-  for(k in 1:5) {
+  for(k in 1:length(bge.mus)) {
     print(paste("Parameter:", k))
     bge.par <- bge.mus[k]
-    gp.par <- gp.pars[k]
-    dib.par <- dib.pars[k]
-    pc.par <- pc.pars[k]
+    alpha_w  <- n + bge.par + 1      
+    t <- bge.par * (alpha_w - n - 1) / (bge.par + 1)
     
     for (i in 1:iter) {
       print(paste("Iteration:", i))
@@ -45,8 +47,20 @@ for(lambda in lambdas) {
       data <- Fou_nldata(truegraph, N, lambda = lambda, noise.sd = 1, standardize = T) 
       
       # Set initial search spaces
-      GP.searchspace = set.searchspace(data, dual, "GP", gp.par)
-      bge.searchspace = set.searchspace(data, dual, "bge", bge.par)
+      DP.searchspace = set.searchspace(data, 
+                                       dual, 
+                                       "DP", 
+                                       usrpar = list(pctesttype = "bge",
+                                                     am = bge.par, 
+                                                     aw = alpha_w, 
+                                                     T0scale = t,
+                                                     dp_iter = 400,
+                                                     burnin = 200,
+                                                     L = 5,
+                                                     edgepf = 1
+                                       )
+      )
+      bge.searchspace = set.searchspace(data, dual, "bge", bgepar = list(am = bge.par))
       
       # Bge score, partition
       bge.fit <- bge.partition.mcmc(bge.searchspace, order = F)
@@ -56,38 +70,43 @@ for(lambda in lambdas) {
       bge.fit <- bge.partition.mcmc(bge.searchspace, order = T)
       results <- compare_results(bge.fit, c(bge.par, "BGe, order", lambda), results, truegraph)
       
-      # GP score, partition
-      gp.ofit <- GP.partition.mcmc(data, GP.searchspace, order = F)
-      results <- compare_results(gp.ofit, c(gp.par, "GP, partition", lambda), results, truegraph)
+      # DP score, partition
+      dp.fit <- DP.partition.mcmc(DP.searchspace, order = F)
+      results <- compare_results(dp.fit, c(bge.par, "DP, partition", lambda), results, truegraph)
       
-      # Laplace score, partition
-      gp.ofit$weights <- NULL
-      gp.ofit$time <- gp.ofit$time - gp.ofit$time2
-      results <- compare_results(gp.ofit, c(gp.par, "Laplace, partition", lambda), results, truegraph)
-      
-      # GP score, order
-      gp.pfit <- GP.partition.mcmc(data, GP.searchspace, order = T)
-      results <- compare_results(gp.pfit, c(gp.par, "GP, order", lambda), results, truegraph)
-      
-      # Laplace score, order
-      gp.pfit$weights <- NULL
-      gp.pfit$time <- gp.pfit$time - gp.pfit$time2
-      results <- compare_results(gp.pfit, c(gp.par, "Laplace, order", lambda), results, truegraph)
-      
-      # DIBS+
-      dib.fit <- DiBS(data, par = dib.par)
-      results <- compare_results(dib.fit, c(dib.par, "DiBS+", lambda), results, truegraph)
-      
-      # k-PC, distance correlation
-      kPC.dcc.fit <- kPC.dcc.boot(data, nboots = 100, alpha = pc.par)
-      results <- compare_results(kPC.dcc.fit, c(pc.par, "kPC-DC", lambda), results, truegraph)
-      
-      # k-PC, HSIC
-      kPC.hsic.fit <- kPC.hsic.boot(data, nboots = 100, alpha = pc.par)
-      results <- compare_results(kPC.hsic.fit, c(pc.par, "kPC-HSIC", lambda), results, truegraph)
+      # DP score, order
+      dp.fit <- DP.partition.mcmc(DP.searchspace, order = T)
+      results <- compare_results(dp.fit, c(bge.par, "DP, order", lambda), results, truegraph)
     }
   }
 }
 colnames(results) <- c("ESHD", "eTP", "eFP", "TPR", "FPR_P", 
                        "time", "parameter", "method", "lambda", "graph")
 saveRDS(results, "Results/Sims_Results_dp.rds")
+
+# plots resutts
+keep_methods <- c("DP, partition", "DP, order", "BGe, partition", "BGe, order")
+
+results_small <- results %>%
+  filter(graph == "dag", method %in% keep_methods) %>%
+  mutate(
+    lambda = factor(as.character(round(as.numeric(lambda), 2)),
+                    levels = c("1", "0.5", "0")),
+    method = factor(method, levels = keep_methods)
+  )
+results_small <- results_small %>%
+  mutate(ESHD = as.numeric(ESHD))
+
+lab_lambda <- function(x) parse(text = paste0("lambda==", x))
+ggplot(results_small, aes(x = method, y = ESHD, color = method)) +
+  geom_boxplot(aes(group = method), width = 0.6, outlier.shape = NA, linewidth = 0.6) +
+  geom_jitter(aes(group = method), width = 0.15, alpha = 0.7, size = 1.2) +
+  facet_wrap(~ lambda, nrow = 1, labeller = labeller(lambda = \(x) paste0("λ = ", x))) +
+  coord_cartesian(ylim = c(0, 20)) +
+  labs(x = NULL, y = "E=SHD") +
+  theme_bw() +
+  theme(
+    legend.position = "bottom",
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+    panel.grid.major.x = element_blank()
+  )
