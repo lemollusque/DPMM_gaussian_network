d93ce6faaf919358fd1afd474c17ba03ba3dae38
bastian widmer <lemolllusque@gmail.com>
2026-03-18 15:18:48 +0100

    Save files at each iterations

--- GP_sims dp only.R
+++ GP_sims dp only.R
@@ -18,28 +18,44 @@ source("dao.R")
 source("fns.R")
 insertSource("fns.R", package = "BiDAG")
 
-
 init.seed <- 100
-iter <- 30
+iter <- 10
 dual <- FALSE
 
 # dirichlet params
-alpha_prior = c(2, 4)
-dp_iter = 100
-dp_fits = 1
-burnin = 90
-L = 10
+alpha_prior <- c(2, 4)
+dp_iter <- 100
+dp_fits <- 1
+burnin <- 90
+L <- 10
 
 param_grid <- expand.grid(
   N = c(200),
   n = 4,
-  d = c(1,2,5,10),
+  d = c(0, 1, 2, 5, 10),
   bge.par = 1
 )
+
 sim_grid <- expand.grid(
   j = seq_len(nrow(param_grid)),
   i = seq_len(iter)
 )
+
+make_job_seed <- function(init.seed, k) {
+  init.seed + k
+}
+
+make_file_name <- function(N, n, d, bge.par, i) {
+  paste0(
+    "Sims/",
+    "N", N,
+    "_n", n,
+    "_d", d,
+    "_bge", bge.par,
+    "_rep", sprintf("%03d", i),
+    ".rds"
+  )
+}
 
 n_cores <- max(1, availableCores() - 1)
 
@@ -50,13 +66,12 @@ registerDoRNG(init.seed)
 handlers(global = TRUE)
 handlers("txtprogressbar")
 
-results <- with_progress({
+with_progress({
   p <- progressor(steps = nrow(sim_grid))
   
   foreach(
     k = seq_len(nrow(sim_grid)),
-    .combine = rbind,
-    .packages = c("BiDAG", "matrixStats", "dirichletprocess", "dplyr", "mclust")
+    .packages = c("BiDAG", "matrixStats", "dirichletprocess", "dplyr", "mclust", "mvtnorm")
   ) %dorng% {
     
     source("comparison_algs.R")
@@ -64,7 +79,7 @@ results <- with_progress({
     source("dao.R")
     source("fns.R")
     insertSource("fns.R", package = "BiDAG")
-
+    
     # set params
     j <- sim_grid$j[k]
     i <- sim_grid$i[k]
@@ -74,41 +89,61 @@ results <- with_progress({
     d <- param_grid$d[j]
     bge.par <- param_grid$bge.par[j]
     
-    g <- er_dag(n, d=0.2)
+    file_name <- make_file_name(
+      N = N,
+      n = n,
+      d = d,
+      bge.par = bge.par,
+      i = i
+    )
+    
+    # resume logic: skip completed jobs
+    if (file.exists(file_name)) {
+      p(sprintf("skip %d", k))
+      return(NULL)
+    }
+    
+    # deterministic per-job seed
+    job_seed <- make_job_seed(init.seed, k)
+    set.seed(job_seed)
+    
+    g <- er_dag(n, d = 0.2)
     g <- sf_out(g)
     truegraph <- randomize_graph(g)
     
-    model1 <- cov(truegraph)
-    model2 <- cov(truegraph)
+    model1 <- cov(truegraph, lb_b = 1, ub_b = 2)
+    model2 <- cov(truegraph, lb_b = 1, ub_b = 2)
     
-    
-    X1 <- rmvt(N/2, sigma =  model1$S, df = 3)
-    X2 <- rmvt(N/2, sigma =  model2$S, df = 3)
+    X1 <- rmvnorm(N / 2, sigma = model1$S)
+    X2 <- rmvnorm(N / 2, sigma = model2$S)
     
     v <- rnorm(ncol(X2))
-    v <- v / sqrt(sum(v^2))   
+    v <- v / sqrt(sum(v^2))
     shift <- d * v
     X2 <- sweep(X2, 2, shift, "+")
     
     data <- standardize(rbind(X1, X2))
-    if(is.null(colnames(data))) {
-      colnames(data) <- sapply(c(1:ncol(data)), function(x) paste("v", x, sep = ""))
+    if (is.null(colnames(data))) {
+      colnames(data) <- paste0("v", seq_len(ncol(data)))
     }
     
-    # prepare dirichlet gamm list
+    # prepare dirichlet gamma list
     Gamma_list <- list()
-    for (f in seq_len(dp_fits)){
-      dp <-  DirichletProcessMvnormal(data, alphaPriors = alpha_prior)
+    for (f in seq_len(dp_fits)) {
+      dp <- DirichletProcessMvnormal(data, alphaPriors = alpha_prior)
       dp <- Fit(dp, dp_iter, progressBar = FALSE)
       
       Gamma_sample <- dp_membership_probs(dp, burnin, L)
-      Gamma_list <- add_membershipp(Gamma_list, 
-                                    Gamma_sample, 
-                                    child=colnames(data)[1], 
-                                    parents=colnames(data)[2:ncol(data)], 
-                                    active=TRUE)
+      Gamma_list <- add_membershipp(
+        Gamma_list,
+        Gamma_sample,
+        child = colnames(data)[1],
+        parents = colnames(data)[2:ncol(data)],
+        active = TRUE
+      )
     }
-    dp_usrpar = list(
+    
+    dp_usrpar <- list(
       pctesttype = "bge",
       am = bge.par,
       membershipp_list = Gamma_list,
@@ -117,7 +152,7 @@ results <- with_progress({
     
     # search spaces
     DP.searchspace <- set.searchspace(data, dual, "DP", usrpar = dp_usrpar)
-    bge.searchspace = set.searchspace(data, dual, "bge", bge.par)
+    bge.searchspace <- set.searchspace(data, dual, "bge", bge.par)
     
     iter_results <- data.frame()
     
@@ -144,23 +179,27 @@ results <- with_progress({
     iter_results$N <- N
     iter_results$n <- n
     iter_results$d <- d
+    iter_results$rep <- i
+    iter_results$bge.par <- bge.par
+    iter_results$job_id <- k
+    iter_results$job_seed <- job_seed
     
-    # show progress
-    p()
+    colnames(iter_results) <- c(
+      "ESHD", "eTP", "eFP", "TPR", "FPR_P",
+      "time", "parameter", "method", "graph",
+      "N", "n", "d", "rep", "bge.par", "job_id", "job_seed"
+    )
     
-    iter_results
+    saveRDS(iter_results, file_name)
+    
+    p(sprintf("done %d", k))
   }
 })
+
 future::plan(sequential)
 
-colnames(results) <- c(
-  "ESHD", "eTP", "eFP", "TPR", "FPR_P",
-  "time", "parameter", "method", "graph", "N", "n", "d"
-)
-
-
-saveRDS(results, "Results/Sims_Results_dp.rds")
-
+files <- list.files("Sims", pattern = "\\.rds$", full.names = TRUE)
+results <- bind_rows(lapply(files, readRDS))
 
 # plots resutts
 keep_methods <- c("DP, partition", "DP, order", "BGe, partition", "BGe, order")
