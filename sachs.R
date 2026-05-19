cf9ebc6b2d607e4009bff82d6960526d1de7d2f7
bastian widmer <lemolllusque@gmail.com>
2026-05-19 11:21:54 +0200

    Plot sachs data

--- sachs.R
+++ sachs.R
@@ -12,24 +12,64 @@ library(progressr)
 library(doRNG)
 library(mvtnorm)
 library(readxl)
+library(GGally)
+library(igraph)
 
 source("comparison_algs.R")
 source("dualPC.R")
-source("Fourier_fns.R")
 source("dao.R")
 source("fns.R")
 insertSource("fns.R", package = "BiDAG")
 
-sachs.data <- read_excel("Sachs/1. cd3cd28.xls")
-trueDAGbn <- readRDS("Sachs/sachs_graph.rds")
+sachs.data <- read.csv("Sachs/2005_sachs_2_cd3cd28icam2_log_std.csv")
+sachs.data <- as.matrix(sachs.data)
+
+# plot data
+my_smooth <- function(data, mapping, ...) {
+  ggplot(data = data, mapping = mapping) +
+    geom_point(color = "black", alpha = 0.5, size = 0.6) +
+    geom_smooth(
+      method = "lm",
+      se = FALSE,
+      color = "red",
+      linewidth = 0.8
+    )
+}
+
+ggpairs(
+  sachs.data,
+  upper = list(continuous = wrap("cor", size = 4)),
+  lower = list(continuous = my_smooth),
+  diag = list(continuous = wrap("barDiag", bins = 30, alpha = 0.5))
+) +
+  theme_minimal()
+
+trueDAG <- read.csv("Sachs/sachs.csv")
+trueDAG <- as.matrix(trueDAG)
+
+# plot DAG
+g <- graph_from_adjacency_matrix(
+  trueDAG,
+  mode = "undirected",
+  diag = FALSE
+)
+
+plot(
+  g,
+  layout = layout_with_fr(g),   # Fruchterman-Reingold
+  vertex.size = 25,
+  vertex.color = "white",
+  vertex.frame.color = "black",
+  vertex.label.cex = 0.8,
+  edge.width = 1.5
+)
+
 set.seed(100)
-sachs.data <- scale(log2(sachs.data + 0.5))
-sachs.data <- standardize(sachs.data)
 
 results <- data.frame()
 
 bge.par = 0.01
-dual <- TRUE
+dual <- TRUE 
 # dirichlet params
 dp_iter <- 1000
 burnin <- 500
