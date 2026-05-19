fcc9b40b6a836863dca9f2e035b4deedf6fe71d5
bastian widmer <lemolllusque@gmail.com>
2026-05-19 11:20:10 +0200

    Parallelize dps for sachs

--- scicore/sachs.R
+++ scicore/sachs.R
@@ -20,22 +20,17 @@ source("fns.R")
 insertSource("fns.R", package = "BiDAG")
 
 sachs.data <- read.csv("Sachs/2005_sachs_2_cd3cd28icam2_log_std.csv")
-
-sachs.data <- standardize(sachs.data)
 sachs.data <- as.matrix(sachs.data)
 trueDAGbn <- read.csv("Sachs/sachs.csv")
 trueDAGbn <- as.matrix(trueDAGbn)
-set.seed(100)
-
-results <- data.frame()
 
 bge.par = 0.01
-dual <- TRUE
+dual <- TRUE 
 # dirichlet params
-dp_iter <- 1000
-burnin <- 500
-L <- 100
-dp_fits <- 2
+dp_iter <- 100
+burnin <- 50
+L <- 10
+dp_fits <- 1
 
 # dp settings
 dp_usrpar <- list(
@@ -47,6 +42,136 @@ dp_usrpar <- list(
   dp_fits = dp_fits
 )
 
-# search spaces
-DP.searchspace <- set.searchspace(sachs.data, dual, "DP", usrpar = dp_usrpar)
-saveRDS(DP.searchspace, "Sachs/dpsearchspace_sachs_2.rds")
+init.seed <- 100
+iter <- 20
+
+dir.create("Sachs/parallel_results", showWarnings = FALSE, recursive = TRUE)
+
+make_file_name <- function(i) {
+  paste0("Sachs/parallel_results/sachs_rep_", sprintf("%03d", i), ".rds")
+}
+
+make_job_seed <- function(init.seed, i) {
+  init.seed + i
+}
+
+n_cores <- max(1, availableCores() - 1)
+
+plan(multisession, workers = n_cores)
+registerDoFuture()
+registerDoRNG(init.seed)
+
+handlers(global = TRUE)
+handlers("txtprogressbar")
+
+with_progress({
+  p <- progressor(steps = iter)
+  
+  foreach(
+    i = seq_len(iter),
+    .packages = c(
+      "BiDAG", "matrixStats", "dirichletprocess", "dplyr",
+      "mclust", "mvtnorm"
+    )
+  ) %dopar% {
+    
+    source("comparison_algs.R")
+    source("dualPC.R")
+    source("dao.R")
+    source("fns.R")
+    insertSource("fns.R", package = "BiDAG")
+    
+    file_name <- make_file_name(i)
+    
+    if (file.exists(file_name)) {
+      p(sprintf("skip %d", i))
+      return(NULL)
+    }
+    
+    set.seed(make_job_seed(init.seed, i))
+    
+    iter_results <- data.frame()
+    
+    DP.searchspace <- set.searchspace(
+      sachs.data,
+      dual,
+      "DP",
+      usrpar = dp_usrpar
+    )
+    
+    bge.searchspace <- set.searchspace(
+      sachs.data,
+      dual,
+      "bge",
+      bge.par
+    )
+    
+    # DP score, partition
+    dp.fit <- DP.partition.mcmc(
+      DP.searchspace,
+      order = FALSE,
+      iterations = 1200
+    )
+    dp.edgep <- post.edges(dp.fit)
+    iter_results <- compare_results(
+      dp.fit,
+      c(dp.edgep, "DP, partition"),
+      iter_results,
+      trueDAG
+    )
+    
+    # DP score, order
+    dp.fit <- DP.partition.mcmc(
+      DP.searchspace,
+      order = TRUE,
+      iterations = 1200
+    )
+    dp.edgep <- post.edges(dp.fit)
+    iter_results <- compare_results(
+      dp.fit,
+      c(dp.edgep, "DP, order"),
+      iter_results,
+      trueDAG
+    )
+    
+    # BGe score, partition
+    bge.fit <- bge.partition.mcmc(
+      bge.searchspace,
+      order = FALSE,
+      iterations = 1200
+    )
+    bge.edgep <- post.edges(bge.fit)
+    iter_results <- compare_results(
+      bge.fit,
+      c(bge.edgep, "BGe, partition"),
+      iter_results,
+      trueDAG
+    )
+    
+    # BGe score, order
+    bge.fit <- bge.partition.mcmc(
+      bge.searchspace,
+      order = TRUE,
+      iterations = 1200
+    )
+    bge.edgep <- post.edges(bge.fit)
+    iter_results <- compare_results(
+      bge.fit,
+      c(bge.edgep, "BGe, order"),
+      iter_results,
+      trueDAG
+    )
+    
+    colnames(iter_results) <- c(
+      "ESHD", "eTP", "eFP", "TPR", "FPR_P", "time",
+      "ErktoAkt", "ErktoPKA", "Scorefn", "graph"
+    )
+    
+    iter_results$rep <- i
+    iter_results$job_seed <- make_job_seed(init.seed, i)
+    
+    saveRDS(iter_results, file_name)
+    
+    p(sprintf("done %d", i))
+  }
+})
