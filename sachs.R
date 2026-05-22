9b0d63aebdd3010d03779b0031a95396481f7fcd
bastian widmer <lemolllusque@gmail.com>
2026-05-22 10:58:51 +0200

    Compute EHSD from DP files

--- sachs.R
+++ sachs.R
@@ -24,96 +24,48 @@ insertSource("fns.R", package = "BiDAG")
 sachs.data <- read.csv("Sachs/2005_sachs_2_cd3cd28icam2_log_std.csv")
 sachs.data <- as.matrix(sachs.data)
 
-# plot data
-my_smooth <- function(data, mapping, ...) {
-  ggplot(data = data, mapping = mapping) +
-    geom_point(color = "black", alpha = 0.5, size = 0.6) +
-    geom_smooth(
-      method = "lm",
-      se = FALSE,
-      color = "red",
-      linewidth = 0.8
-    )
-}
-
-ggpairs(
-  sachs.data,
-  upper = list(continuous = wrap("cor", size = 4)),
-  lower = list(continuous = my_smooth),
-  diag = list(continuous = wrap("barDiag", bins = 30, alpha = 0.5))
-) +
-  theme_minimal()
-
 trueDAG <- read.csv("Sachs/sachs.csv")
 trueDAG <- as.matrix(trueDAG)
 
-# plot DAG
-g <- graph_from_adjacency_matrix(
-  trueDAG,
-  mode = "undirected",
-  diag = FALSE
-)
-
-plot(
-  g,
-  layout = layout_with_fr(g),   # Fruchterman-Reingold
-  vertex.size = 25,
-  vertex.color = "white",
-  vertex.frame.color = "black",
-  vertex.label.cex = 0.8,
-  edge.width = 1.5
-)
-
 set.seed(100)
-
-results <- data.frame()
 
 bge.par = 0.01
 dual <- TRUE 
-# dirichlet params
-dp_iter <- 1000
-burnin <- 500
-L <- 100
-dp_fits <- 4
 
-# dp settings
-dp_usrpar <- list(
-  pctesttype = "bge",
-  am = bge.par,
-  dp_iter = dp_iter,
-  dp_burnin = burnin,
-  dp_n_sample = L,
-  dp_fits = dp_fits
+results <- data.frame()
+
+files <- list.files(
+  "Sachs/parallel_searchspaces",
+  pattern = "\\.rds$",
+  full.names = TRUE
 )
 
-# search spaces
-# DP.searchspace <- set.searchspace(sachs.data, dual, "DP", usrpar = dp_usrpar)
-# saveRDS(DP.searchspace, "Sachs/dpsearchspace.rds")
-DP.searchspace <- readRDS("Sachs/dpsearchspace.rds")
-
-bge.searchspace = set.searchspace(sachs.data, dual, "bge", bge.par)
-
-for (i in 1:100){
-  print(i)
+for (file in files){
+  print(file)
+  
+  DP.searchspace <- readRDS(file)
+  bge.searchspace <- set.searchspace(sachs.data, dual, "bge", bge.par)
+  
+  
   # DP score, partition
   dp.fit <- DP.partition.mcmc(DP.searchspace, order = FALSE, iterations = 1200)
   dp.edgep <- post.edges(dp.fit)
-  results <- compare_results(dp.fit, c(dp.edgep, "DP, partition"), results, trueDAGbn)
+  results <- compare_results(dp.fit, c(dp.edgep, "DP, partition"), results, trueDAG)
   
   # DP score, order
   dp.fit <- DP.partition.mcmc(DP.searchspace, order = TRUE, iterations = 1200)
   dp.edgep <- post.edges(dp.fit)
-  results <- compare_results(dp.fit, c(dp.edgep, "DP, order"), results, trueDAGbn)
+  results <- compare_results(dp.fit, c(dp.edgep, "DP, order"), results, trueDAG)
   
   # BGe score, partition
   bge.fit <-  bge.partition.mcmc(bge.searchspace, order = F, iterations = 1200)
   bge.edgep <- post.edges(bge.fit)
-  results <- compare_results(bge.fit, c(bge.edgep, "BGe, partition"), results, trueDAGbn)
+  results <- compare_results(bge.fit, c(bge.edgep, "BGe, partition"), results, trueDAG)
   
   # BGe score, order
   bge.fit <-  bge.partition.mcmc(bge.searchspace, order = T, iterations = 1200)
   bge.edgep <- post.edges(bge.fit)
-  results <- compare_results(bge.fit, c(bge.edgep, "BGe, order"), results, trueDAGbn)
+  results <- compare_results(bge.fit, c(bge.edgep, "BGe, order"), results, trueDAG)
 }
 
 colnames(results) <- c("ESHD", "eTP", "eFP", "TPR", "FPR_P", "time",
