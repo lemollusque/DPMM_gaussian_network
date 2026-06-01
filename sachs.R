f2c41561d9b75c862a5e826d667a99116fe766bf
bastian widmer <lemolllusque@gmail.com>
2026-06-01 16:11:53 +0200

    Use dp.edgep in sachs

--- sachs.R
+++ sachs.R
@@ -60,16 +60,20 @@ results_list <- foreach(
   res <- data.frame()
   
   dp.fit <- DP.partition.mcmc(DP.searchspace, order = FALSE, iterations = 1200)
-  res <- compare_results(dp.fit, c("DP, partition"), res, trueDAG)
+  dp.edgep <- post.edges(dp.fit, weights = dp.fit$weights)
+  res <- compare_results(dp.fit, c(dp.edgep, "DP, partition"), res, trueDAG)
   
   dp.fit <- DP.partition.mcmc(DP.searchspace, order = TRUE, iterations = 1200)
-  res <- compare_results(dp.fit, c("DP, order"), res, trueDAG)
+  dp.edgep <- post.edges(dp.fit, weights = dp.fit$weights)
+  res <- compare_results(dp.fit, c(dp.edgep, "DP, order"), res, trueDAG)
   
   bge.fit <- bge.partition.mcmc(bge.searchspace, order = FALSE, iterations = 1200)
-  res <- compare_results(bge.fit, c("BGe, partition"), res, trueDAG)
+  bge.edgep <- post.edges(bge.fit)
+  res <- compare_results(bge.fit, c(bge.edgep, "BGe, partition"), res, trueDAG)
   
   bge.fit <- bge.partition.mcmc(bge.searchspace, order = TRUE, iterations = 1200)
-  res <- compare_results(bge.fit, c("BGe, order"), res, trueDAG)
+  bge.edgep <- post.edges(bge.fit)
+  res <- compare_results(bge.fit, c(bge.edgep, "BGe, order"), res, trueDAG)
   
   res$file <- basename(file)
   res
@@ -77,10 +81,8 @@ results_list <- foreach(
 
 results <- dplyr::bind_rows(results_list)
 
-colnames(results)[1:8] <- c(
-  "ESHD", "eTP", "eFP", "TPR", "FPR_P", "time",
-  "Scorefn", "graph"
-)
+colnames(results) <- c("ESHD", "eTP", "eFP", "TPR", "FPR_P", "time",
+                       "ErktoAkt", "ErktoPKA", "Scorefn", "graph", "file")
 
 saveRDS(results, "Sachs/Sachs_results.rds")
 
