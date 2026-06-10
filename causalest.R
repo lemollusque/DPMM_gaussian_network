aaf6626d6b0652625d5bfabeb1109cf6513c2c10
bastian widmer <lemolllusque@gmail.com>
2026-06-10 12:10:06 +0200

    Compute effects without intermediate saves

--- causalest.R
+++ causalest.R
@@ -29,7 +29,7 @@ source("dao.R")
 source("fns.R")
 insertSource("fns.R", package = "BiDAG")
 
-set.seed(7)
+set.seed(9)
 N = 100
 n = 10
 # generate graph
@@ -43,6 +43,8 @@ sim <- simulate_bimodal(
   bimodal_sep = 5,
   return_model = TRUE
 )
+
+truegraph = t(sim$detectable_truegraph)
 
 data <- sim$data
 if (is.null(colnames(data))) {
@@ -80,18 +82,18 @@ sampleTrueEffects <- function(Eff1, Eff2, n1, n2, labels = NULL, nSamples = 1000
   }
   out
 }
-trueSamples <- sampleTrueEffects(
+trueEffectSamples <- sampleTrueEffects(
   Eff1 = Eff1,
   Eff2 = Eff2,
   n1 = sim$n1,
   n2 = sim$n2,
-  labels = colnames(sim$data),
+  labels = colnames(truegraph),
   nSamples = 1000
 )
 # plot
-pdf("TrueEffects.pdf", width = 6, height = 6)
+pdf("effects/TrueEffects.pdf", width = 6, height = 6)
 plotEffects(
-  effects4plot = trueSamples, xmargs = c(0.1, 0.3), label_size = 1.5,
+  effects4plot = trueEffectSamples, xmargs = c(0.1, 0.3), label_size = 1.5,
   sortlabs = 1:n,
   title_text = ""
 )
@@ -108,130 +110,72 @@ sim$n2
 ##############################################
 # with perfect dags
 ##############################################
-makeTrueDAGSamples <- function(truegraph, nDAGs, seed = 101, dname = "",
-                               scoreObject, outdir = "./saveout") {
-  
-  if (!dir.exists(outdir)) {
-    dir.create(outdir, recursive = TRUE)
-  }
-  
-  n <- ncol(truegraph)
-  
-  sampledDAGs <- replicate(
-    nDAGs + 1,
-    truegraph,
-    simplify = FALSE
-  )
-  
-  sampledDAGs <- lapply(sampledDAGs, function(A) {
+makeTrueDAGSamples <- function(truegraph, nDAGs) {
+  sampledDAGs <- replicate(nDAGs + 1, truegraph, simplify = FALSE)
+    sampledDAGs <- lapply(sampledDAGs, function(A) {
     A <- as.matrix(A)
-    colnames(A) <- rownames(A) <- colnames(scoreObject$data)
+    colnames(A) <- rownames(A) <- colnames(truegraph)
     A
   })
   
-  DAGscores <- rep(NA_real_, length(sampledDAGs))
-  
-  save(
-    sampledDAGs,
-    DAGscores,
-    scoreObject,
-    file = file.path(outdir, paste0("dagdraw", n, "seed", seed, dname, ".RData"))
-  )
+  sampledDAGs
+}
+computeEffects_mem <- function(sampledDAGs, scoreObject, DP = FALSE) {
+  if (DP) {
+    DP_DAGintervention(
+      incidences = sampledDAGs,
+      dataParams = scoreObject,
+      sample = TRUE
+    )
+  } else {
+    DAGintervention(sampledDAGs, scoreObject, sample = TRUE)
+  }
 }
 
-nDAGs <- 50
-nSeeds <- 50
-seeds_per_dp <- 10
-batch <- 100 + 1:nSeeds
+nDAGs <- 500
 
-dp_groups <- split(
-  batch,
-  ceiling(seq_along(batch) / seeds_per_dp)
-)
-
-labels4plot <- colnames(data) 
-nNodes <- length(labels4plot)
-
-# dp settings
+bge.par = 0.01
+# dirichlet params
 dp_usrpar <- list(
   pctesttype = "bge",
-  am = 0.01,
-  dp_iter = 200,
-  dp_burnin = 100,
-  dp_n_sample = 10,
+  am = bge.par,
+  dp_prior = list(strength = 0.1, discount = 0),
+  dp_mcmc = list(niter = 5000, nburn = 3000, model="LS"),
+  dp_n_sample = 20,
   dp_fits = 1,
-  alphaPriors = c(8,4),
-  g0Priors = function(n) {
-    list(mu0 = rep(0, n), 
-         kappa0 = 0.1, 
-         nu = n+5, 
-         Lambda = diag(n)*0.5)
-  },
-  numInitialClusters = min(20, ceiling(sqrt(N))),
-  progressBar = T
+  dp_fitspace = "full"
 )
 
-plan(multisession, workers = min(length(dp_groups), parallelly::availableCores() - 1))
-registerDoFuture()
+DP.searchspace <- set.searchspace(
+  data,
+  "DP",
+  usrpar = dp_usrpar
+)
 
-handlers(global = TRUE)
+# prepare effects
+trueSamples = makeTrueDAGSamples(
+  truegraph = truegraph,
+  nDAGs = nDAGs
+)
 
-foreach(
-  g = seq_along(dp_groups),
-  .packages = c("BiDAG", "matrixStats", "dirichletprocess", "dplyr", "mclust", "mvtnorm")
-  ) %dorng% {
-  
-  source("toyDAGfunctionsSachs.R")
-  source("comparison_algs.R")
-  source("dualPC.R")
-  source("Fourier_fns.R")
-  source("dao.R")
-  source("fns.R")
-  insertSource("fns.R", package = "BiDAG")
-  
-  cat("Fitting DP group", g, "\n")
-  
-  DP.searchspace <- set.searchspace(
-    data,
-    "dual",
-    "DP",
-    usrpar = dp_usrpar
-  )
-  
-  for (seednumber in dp_groups[[g]]) {
-    
-    cat("Seed is", seednumber, "\n")
-    
-    makeTrueDAGSamples(
-      truegraph = truegraph,
-      nDAGs = nDAGs,
-      seed = seednumber,
-      scoreObject = DP.searchspace$score,
-      outdir = "./Sachs/saveout"
-    )
-    
-    computeEffects(
-      n = nNodes,
-      seed = seednumber,
-      DP = TRUE
-    )
-  }
-  
-  TRUE
-}
-data4plot <- loadsamples(seeds=batch, nn=nNodes)
+alleffs = computeEffects_mem(
+  sampledDAGs = trueSamples,
+  scoreObject = DP.searchspace$score,
+  DP = TRUE
+)
 
-pdf("EstimatedEffects.pdf", width = 6, height = 6)
-plotEffects(effects4plot = data4plot$alleffs, xmargs = c(0.1, 0.3), label_size = 1.5,
+# plot effects
+pdf("effects/EstimatedEffects.pdf", width = 6, height = 6)
+plotEffects(effects4plot = alleffs, xmargs = c(0.1, 0.3), label_size = 1.5,
             sortlabs = 1:n, 
             title_text = "")
 dev.off()
 
-pdf("Wasserstein.pdf", width = 6, height = 6)
+pdf("effects/Wasserstein.pdf", width = 6, height = 6)
 # compare with true effects
 Wdist <- plotCompareEffects(
-  effects4plot = data4plot$alleffs,
-  trueEffects = trueSamples,
+  effects4plot = alleffs,
+  trueEffects = trueEffectSamples,
   sortlabs = 1:n,
   title_text = ""
 )
@@ -239,30 +183,22 @@ dev.off()
 
 #######################################################################
 # bge
+bge.searchspace <- set.searchspace(data, "bge", 0.01)
+bge.alleffs = computeEffects_mem(sampledDAGs = trueSamples,
+                   scoreObject = bge.searchspace$score,
+                   DP=F) 
 
-for (seednumber in batch) { 
-  print(paste("Seed is", seednumber)) 
-  # search spaces 
-  bge.searchspace <- set.searchspace(data, TRUE, "bge", 0.01)
-  makeTrueDAGSamples( truegraph = truegraph, 
-                      nDAGs = nDAGs, seed = seednumber, 
-                      scoreObject = bge.searchspace$score, 
-                      outdir = "./Sachs/saveout" ) 
-  computeEffects(n = nNodes, seed = seednumber, DP=F) 
-}
-data4plot <- loadsamples(seeds=batch, nn=nNodes)
-
-pdf("BGeEstimatedEffects.pdf", width = 6, height = 6)
-plotEffects(effects4plot = data4plot$alleffs, xmargs = c(0.1, 0.3), label_size = 1.5,
+pdf("effects/BGeEstimatedEffects.pdf", width = 6, height = 6)
+plotEffects(effects4plot = bge.alleffs, xmargs = c(0.1, 0.3), label_size = 1.5,
             sortlabs = 1:n, 
             title_text = "")
 dev.off()
 
-pdf("BGeWasserstein.pdf", width = 6, height = 6)
+pdf("effects/BGeWasserstein.pdf", width = 6, height = 6)
 # compare with true effects
 Wdist <- plotCompareEffects(
-  effects4plot = data4plot$alleffs,
-  trueEffects = trueSamples,
+  effects4plot = bge.alleffs,
+  trueEffects = trueEffectSamples,
   sortlabs = 1:n,
   title_text = ""
 )
