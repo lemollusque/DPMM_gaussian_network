ab3392a43ef936b20ff1bba569ccac3689278f95
bastian widmer <lemolllusque@gmail.com>
2026-06-28 10:39:02 +0200

    Set up ibge on slurm

---
+++ scicore/isachscausal.R
@@ -0,0 +1,72 @@
+# preprocessing etc of the Sachs data
+source("./isachssetup.R")
+
+# load libraries
+library(BiDAG)
+library(Bestie)
+
+library(matrixStats)
+library(dplyr)
+library(ggplot2)
+library(foreach)
+library(doFuture)
+library(future)
+library(parallelly)
+library(mclust)
+library(progressr)
+library(doRNG)
+library(mvtnorm)
+library(BayesFactor)
+library(matrixStats)
+library(BNPmix)
+
+# Use BiDAG with intervention scoring
+source("ifnsdp.R")
+insertSource("ifnsdp.R", package = "BiDAG")
+
+# load causal pipeline taken and adapted from https://github.com/annlia/causalpipe
+source("itoyDAGfunctionsSachs.R")
+source("intfns.R")
+
+inputData <- scale(data)
+nDAGs <- 50
+nSeeds <- 50
+batch <- 100 + 1:nSeeds
+labels4plot <- colnames(inputData) 
+nNodes <- length(labels4plot)
+
+dp_usrpar <- list(
+  pctesttype = "bge",
+  am = 0.01,
+  Imat = Imat,
+  dp_prior = list(strength = 0.0002, discount = 0),
+  dp_mcmc = list(niter = 4000, nburn = 3000, model="LS"),
+  dp_n_sample = 100,
+  dp_fits = 2,
+  dp_fitspace = "full",
+  bgremove = TRUE
+)
+
+plan(multisession, workers = min(length(batch), availableCores()))
+registerDoFuture()
+
+foreach(
+  seednumber = batch,
+  .packages = c("BiDAG", "Bestie", "data.table", "mvtnorm", "BNPmix")
+) %dorng% {
+  
+  source("ifnsdp.R")
+  insertSource("ifnsdp.R", package = "BiDAG")
+  source("intfns.R")
+  source("itoyDAGfunctionsSachs.R")
+
+  timing <- proc.time()
+  print(paste("Seed is", seednumber))
+  sampleDAGs(inData=inputData, scoretype = "usr",
+             usrpar = dp_usrpar,
+             nDigraphs=nDAGs, seed=seednumber,
+             weighted=TRUE)
+  computeEffects(n=nNodes, seed=seednumber, DP=TRUE)
+  print(proc.time() - timing)
+  TRUE
+}
